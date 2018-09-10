d7cf2afc117e2a671d7c79f7ac312c2b215cdce6
Rahgoki <barkerjm@me.com>
2018-09-09 20:40:32 -0500

    fixtures

---
+++ Day12/test_mydb.py
@@ -0,0 +1,23 @@
+from fixtures.mydb import MyDB
+
+import pytest
+
+@pytest.fixture(scope="module")
+def cur():
+    print("setting up")
+    db = MyDB()
+    conn = db.connect("server")
+    curs = conn.cursor()
+    yield curs
+    curs.close()
+    conn.close()
+    print("closing connection")
+
+def test_johns_id(cur):
+    id = cur.execute("select id from employee_db where name=John")
+    assert id == 123
+
+
+def test_toms_id(cur):
+    id = cur.execute("select id from employee_db where name=Tom")
+    assert id == 789
